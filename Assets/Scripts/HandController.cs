5e6463d273f0eb1d2f5e2a875fa99a762e141243
KajetanRadulski <hesmeron@gmail.com>
2025-07-04 17:31:50 +0200

    Add Bounds for hands

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -1,11 +1,30 @@
+using System;
 using UnityEngine;
 
 public class HandController : MonoBehaviour
 {
-    [SerializeField] private float _speed = 1f;
+    [SerializeField] 
+    private PlayAreaBounds _bounds;
+    [SerializeField] 
+    private float _speed = 1f;
+    [SerializeField]
+    private Vector2 _target;
+
+    void Awake()
+    {
+        _target = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.Lerp(transform.position, _target, _speed * Time.deltaTime);
+    }
+
     public void ReceiveInput(Vector2 inputValue)
     {
         Vector2 translation = inputValue.normalized * (Time.deltaTime * inputValue.magnitude * _speed);
-        transform.position += new Vector3(translation.x, translation.y, 0);
+        Vector2 newTarget = _bounds.ClampPoint(_target + translation);
+        _target = newTarget;
+        
     }
 }
--- Assets/Scripts/PlayAreaBounds.cs
+++ Assets/Scripts/PlayAreaBounds.cs
@@ -10,4 +10,11 @@ public class PlayAreaBounds : MonoBehaviour
         Gizmos.color = new Color(0, 1,  0, 0.25f);
         Gizmos.DrawCube(transform.position, _extents);
     }
+    
+    public Vector2 ClampPoint(Vector2 value)
+    {
+        float x = Mathf.Clamp(value.x, -_extents.x /2, _extents.x/2);
+        float y = Mathf.Clamp(value.y, -_extents.y/2, _extents.y/2);
+        return new Vector2(x, y);
+    }
 }
